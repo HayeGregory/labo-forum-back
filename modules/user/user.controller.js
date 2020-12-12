75470bc5ba34855d75bfdae25c95e585917ddf58
HayeGregory <haye.greg@outlook.com>
2020-12-12 14:58:26 +0100

    create action  users  ok

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const db = require('../../utils/db.util');
 
 class UserController {
@@ -8,7 +7,7 @@ class UserController {
         res.status(500).json({error: err.message}); 
     };
     
-    // GET
+    // GET en promesse synchrone
     getAllAction(req, res) {
         db.readDB(db.DB_PATH)
             .then(data => res.json(data))
@@ -19,6 +18,23 @@ class UserController {
         db.readDB(db.DB_PATH)
             .then(data => res.json(data.users.find(u => u.id == id)))
             .catch(err => this.#errorResponse(err, res));
+    }
+
+    // CREATE en async (pour faire autrement)
+    async createAction( {body}, res ) {
+        try {
+            const data = await db.readDB(db.DB_PATH);
+            const LastID = Math.max(...data.users.map(u => u.id));
+            const user = body;
+
+            user.id = LastID + 1 ;
+            data.users.push(user);
+            await db.writeDB(db.DB_PATH, data); // ???? pas d'await
+            res.json(user);
+
+        } catch (err) {
+
+        }
     }
 
 }
--- modules/user/user.routes.js
+++ modules/user/user.routes.js
@@ -3,8 +3,11 @@ const UserController = require('./user.controller');
 
 const router = express.Router();
 
-// mes routes de mon controller
+// mes routes de mon controller equivalant [httpGet], annotation en c#
 router.get('/', UserController.getAllAction);
 router.get('/:id([0-9]+)', UserController.getOneByIdAction);
+// pl. route vers mon create Action
+router.post('/', UserController.createAction);
+router.post('/create', UserController.createAction);
 
 module.exports = router;
